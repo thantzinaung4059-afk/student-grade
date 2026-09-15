5187791e5d7bef8b105f1c9f124c6fd7ab713812
thantzinaung4059-afk <thantzinaung4059@gmail.com>
2026-09-15 20:27:15 +0630

    Add total mark calculation

--- grade.py
+++ grade.py
@@ -4,7 +4,18 @@ name = input("Enter student name: ")
 mark1 = float(input("Enter first subject mark: "))
 mark2 = float(input("Enter second subject mark: "))
 
-avg_mark = (mark1 + mark2) / 2
+total_mark = mark1 + mark2
+avg_mark = total_mark / 2
 
 print(f"Student Name: {name}")
+print(f"Total Mark: {total_mark:.0f}")
 print(f"Average Mark: {avg_mark:.0f}")
+
+
+
+
+
+
+
+
+
